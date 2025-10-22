49c36da54ad12f939476830f7a97e7d87331e0e2
HyoYoonNam <dev.rudevico@gmail.com>
2025-10-22 23:22:13 +0900

    refactor(car): 이름 검증 로직을 NameValidator 중첩 클래스로 이동한다
    검증에 대한 '책임' 자체는 여전히 Car 객체가 가진다.
    
    하지만 '책임을 수행하기 위한 구현'인 '로직'은
    NameValidator로 이동한다.
    
    즉, 다음과 같은 표현은 옳지 않다.
    예. 이름 검증 책임을 NameValidator 중첩 클래스로 위임한다

--- src/test/java/racingcar/Car.java
+++ src/test/java/racingcar/Car.java
@@ -4,23 +4,30 @@ import java.util.regex.Pattern;
 
 public class Car {
 
-    private static final String invalidNameRegex = "[^a-zA-Z0-9_ ]|[_ ]{2,}";
-    private static final Pattern invalidNamePattern = Pattern.compile(invalidNameRegex);
-
     public Car(String name) {
-        validateNamePattern(name);
-        validateNameLength(name);
+        NameValidator.validate(name);
     }
 
-    private static void validateNamePattern(String name) {
-        if (invalidNamePattern.matcher(name).find()) {
-            throw new IllegalArgumentException("이름은 5자를 넘을 수 없습니다: " + name);
+    private static class NameValidator {
+
+        private static final String invalidNameRegex = "[^a-zA-Z0-9_ ]|[_ ]{2,}";
+        private static final Pattern invalidNamePattern = Pattern.compile(invalidNameRegex);
+
+        private static void validate(String name) {
+            validateNamePattern(name);
+            validateNameLength(name);
         }
-    }
 
-    private static void validateNameLength(String name) {
-        if (name.length() > 5) {
-            throw new IllegalArgumentException("사용할 수 없는 이름입니다: " + name);
+        private static void validateNamePattern(String name) {
+            if (invalidNamePattern.matcher(name).find()) {
+                throw new IllegalArgumentException("사용할 수 없는 이름입니다: " + name);
+            }
+        }
+
+        private static void validateNameLength(String name) {
+            if (name.length() > 5) {
+                throw new IllegalArgumentException("이름은 5자를 넘을 수 없습니다: " + name);
+            }
         }
     }
 }
