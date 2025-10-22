97bac3906fda64f55f6a33e559fdcbc52e96668b
HyoYoonNam <dev.rudevico@gmail.com>
2025-10-22 16:29:09 +0900

    refactor(separator): @BeforeEach 애노테이션을 사용해 반복되는 테스트 설정을 공통화 한다
    이전에는 각 테스트 메서드마다 CarNameSeparator 객체를 생성하는 코드를 작성해줘야 된다.
    
    CarNameSeparator 타입을 보관하는 변수를 클래스 레벨에 두고, @BeforeEach 애노테이션을 사용한 메서드로 주입해주어 반복을 제거한다.

--- src/test/java/racingcar/CarNameSeparatorTest.java
+++ src/test/java/racingcar/CarNameSeparatorTest.java
@@ -3,16 +3,23 @@ package racingcar;
 import static org.assertj.core.api.Assertions.*;
 
 import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class CarNameSeparatorTest {
 
+    private CarNameSeparator carNameSeparator;
+
+    @BeforeEach
+    void beforeEach() {
+        carNameSeparator = new CarNameSeparator();
+    }
+
     @Test
     @DisplayName("자동차 이름을 쉼표 기준으로 분리한다")
     void separateCarNameByComma() {
         String carname = "pobi,woni,jun";
-        CarNameSeparator carNameSeparator = new CarNameSeparator();
         List<String> carNameList = carNameSeparator.separate(carname);
         assertThat(carNameList).isEqualTo(List.of("pobi", "woni", "jun"));
     }
