e95735d8cd547727b7bb16d0a5acc0280d57cd5c
HyoYoonNam <dev.rudevico@gmail.com>
2025-10-24 18:44:37 +0900

    refactor(game): 검증 로직을 메서드로 추출한다

--- src/test/java/racingcar/RacingGame.java
+++ src/test/java/racingcar/RacingGame.java
@@ -10,15 +10,37 @@ public class RacingGame {
     }
 
     private void init(String carNamesString, int totalRounds) {
+        validateParameters(carNamesString, totalRounds);
+    }
+
+    private static void validateParameters(String carNamesString, int totalRounds) {
+        validateCarNamesString(carNamesString);
+        validateTotalRounds(totalRounds);
+    }
+
+    private static void validateCarNamesString(String carNamesString) {
         CarNameSeparator carNameSeparator = new CarNameSeparator();
         List<String> separated = carNameSeparator.separate(carNamesString);
+
+        validateCarCount(carNamesString, separated);
+
+        validateCarNamesDuplicated(carNamesString, separated);
+    }
+
+    private static void validateCarCount(String carNamesString, List<String> separated) {
         if (separated.size() < 2) {
             throw new IllegalArgumentException("참가하는 자동차 수가 2개 미만입니다. 자동차의 수나 구분자를 확인해주세요: " + carNamesString);
         }
+    }
+
+    private static void validateCarNamesDuplicated(String carNamesString, List<String> separated) {
         Set<String> carNames = Set.copyOf(separated);
         if (separated.size() != carNames.size()) {
             throw new IllegalArgumentException("중복된 자동차 이름이 존재합니다: " + carNamesString);
         }
+    }
+
+    private static void validateTotalRounds(int totalRounds) {
         if (totalRounds < 1) {
             throw new IllegalArgumentException("게임의 총 진행 회차는 1회 이상이어야 합니다: " + totalRounds);
         }
