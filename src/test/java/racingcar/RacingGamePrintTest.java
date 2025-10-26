82490a6450d5efeb9661fa7c04cd765419bbe6ba
HyoYoonNam <dev.rudevico@gmail.com>
2025-10-26 16:37:04 +0900

    test(game): 출력에 대한 테스트 클래스를 별도로 만든다
    System.out 출력을 테스트 하기 위해서는 별도의
    출력 스트림 설정이 필요하다.
    
    하지만 해당 설정은 RacingGame에서 출력이 필요
    하지 않은 로직들은 설정할 이유가 없다.
    
    따라서 출력 스트림 설정과 그 설정이 필요한
    테스트들만 따로 구분하여 관리하기 위해서
    별도의 테스트 클래스를 만든다.

---
+++ src/test/java/racingcar/RacingGamePrintTest.java
@@ -0,0 +1,23 @@
+package racingcar;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+public class RacingGamePrintTest {
+
+    private ByteArrayOutputStream outputStream;
+    private final PrintStream originalOut = System.out;
+
+    @BeforeEach
+    void setUp() {
+        outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
+    }
+
+    @AfterEach
+    public void restoreSystemOut() {
+        System.setOut(originalOut);
+    }
+}
