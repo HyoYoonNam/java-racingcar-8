379df0622604daaf01c9540ee328f663d2edf01d
HyoYoonNam <dev.rudevico@gmail.com>
2025-10-27 14:45:11 +0900

    feat(application): Console 라이브러리 래핑 객체로 입력 받는다

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,12 @@
 package racingcar;
 
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        String carNames = InputView.readCarNames();
+        int totalRounds = InputView.readTotalRounds();
+
+        RacingGame game = new RacingGame(carNames, totalRounds);
+        game.start();
     }
 }
---
+++ src/main/java/racingcar/InputView.java
@@ -0,0 +1,46 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.util.NoSuchElementException;
+
+public class InputView {
+
+    private InputView() {
+    }
+
+    public static String readCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        return readLine();
+    }
+
+    public static int readTotalRounds() {
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        return readInt();
+    }
+
+    private static String readLine() {
+        String line = null;
+
+        try {
+            line = Console.readLine();
+        } catch (NoSuchElementException e) {
+            throw new IllegalArgumentException("입력이 존재하지 않습니다", e);
+        }
+
+        return line;
+    }
+
+    private static int readInt() {
+        int userInput = 0;
+
+        try {
+            userInput = Integer.parseInt(Console.readLine());
+        } catch (NoSuchElementException e) {
+            throw new IllegalArgumentException("입력이 존재하지 않습니다", e);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("입력한 값이 숫자가 아닙니다", e);
+        }
+
+        return userInput;
+    }
+}
